228e0cd651cf0ee85253bb5488a436f99ff5e05c
robertovagner775 <robertovagner04@gmail.com>
2022-12-01 17:08:22 -0300

    adição do codigo do gerador

--- gerador_senha.php
+++ gerador_senha.php
@@ -1,2 +1,58 @@
 <?php
+session_start();
+$_SESSION['password'] = "";
+$quantidade_caracter = $_POST['qtdcaracter'];
+
+$letras_minusculas = range('A', 'Z'); 
+$letras_maiusculas = range('a', 'z'); 
+$numeros = range("0", "9");
+$simbolos = ['!', '@', '#', '%', '%',  '&', '*'];
+
+foreach($letras_minusculas as $chave => $letra){
+    echo($letra);
+}
+
+foreach($letras_maiusculas as $chave => $letra){
+    echo($letra);
+}
+foreach($numeros as $chave => $numero){
+    echo($numero);
+}
+
+foreach($simbolos as $chave => $simbolo){
+    echo($simbolo);
+}
+
+$array_caracter = [];
+if(isset($_POST['maiuscula'])){
+    foreach($letras_maiusculas as $letra){
+        array_push($array_caracter, $letra);
+    }
+ }
+if(isset($_POST['minuscula'])){
+    foreach($letras_minusculas as $letra){
+        array_push($array_caracter, $letra);
+    }
+ }
+ if(isset($_POST['simbolo'])){
+    foreach($simbolos as $simbolo){
+        array_push($array_caracter, $simbolo);
+    }
+ }
+ if(isset($_POST['numero'])){
+    foreach($numeros as $num){
+        array_push($array_caracter, $num);
+    }
+ }
+
+
+$password = "";
+$key = 0;
+for($i = 0; $i<$quantidade_caracter; $i++){
+    $password .= $array_caracter[array_rand($array_caracter)];
+}
+
+
+$_SESSION['password'] = $password;
+header("location: index.php")
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,17 +11,40 @@
 <body>
 
     <div class="container">
+        <?php
+    
+            if(isset($_SESSION['password'])){
+                $pass = $_SESSION['password'];
+            }
+        
+        ?>
         <div> <h3>Gerador de Senhas</h3> </div>
-        <form action="">
+        <form action="gerador_senha.php" method="POST">
 
             <label for="">Quantidade de caracteres</label>
-            <input type="text"> 
+            <input type="text" name="qtdcaracter"> 
             
             <fieldset>
                 <div>
-                    <input type="checkbox" name="maiusculas" id="idMaiusculas">
+                   <label for="">Maiusculas</label>
+                     <input type="checkbox" name="maiuscula" id="idMaiuscula">
                 </div>
-            </fieldset>    
+                <div>
+                     <label for="">Minusculos</label>
+                     <input type="checkbox" name="minuscula" id="idminuscula">
+                </div>
+                <div>
+                     <label for="">Numeros</label>
+                     <input type="checkbox" name="numero" id="idNumero">
+                </div>
+                <div>
+                     <label for="">Simbolos</label>
+                     <input type="checkbox" name="simbolo" id="idSimbolo">
+                </div>
+            </fieldset> 
+        
+            <h4> <?php echo isset($_SESSION["password"]) ? $_SESSION['password'] : "..." ?></h4>
+            <input type="submit" >
         </form>
     </div>
     
