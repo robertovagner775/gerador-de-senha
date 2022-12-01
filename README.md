8cee78e7eaa10f63caf8f54b256058058f7fdb9b
robertovagner775 <robertovagner04@gmail.com>
2022-12-01 11:05:21 -0300

    estruturação do projeto

---
+++ README.md
@@ -0,0 +1,4 @@
+# Gerador de Senhas feito em PHP 
+
+## com opções de adicionar letras maiusculas e minusculas no app.
+## alem de poder adicionar tamanho e palavras chaves.
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gerador de Senha</title>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+
+    <div class="container">
+        <div> <h3>Gerador de Senhas</h3> </div>
+        <form action="">
+
+            <label for="">Quantidade de caracteres</label>
+            <input type="text"> 
+            
+            <fieldset>
+                <div>
+                    <input type="checkbox" name="maiusculas" id="idMaiusculas">
+                </div>
+            </fieldset>    
+        </form>
+    </div>
+    
+</body>
+</html>
